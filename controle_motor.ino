acfde78c77047420b6a875ce012ba23e12ace4ab
Fábio Balbé <fabiobalbe@MacBook-Air-de-Fabio.local>
2023-05-31 15:09:45 -0300

    simplificando codigo

--- controle_motor.ino
+++ controle_motor.ino
@@ -1,5 +1,3 @@
-/*Example sketch to control a stepper motor with A4988/DRV8825 stepper motor driver and Arduino without a library. More info: https://www.makerguides.com */
-// Define stepper motor connections and steps per revolution:
 #define dirPin 2
 #define stepPin 3
 #define stepsPerRevolution 200
@@ -54,4 +52,3 @@ delayMicroseconds(500);
 }
 delay(1000);
 }
-/*Example sketch to control a stepper motor with A4988/DRV8825 stepper motor
